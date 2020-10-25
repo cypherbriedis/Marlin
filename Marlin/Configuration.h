902d5c536befc0140c77bd6a649e0375fabbc9f4
cypherbriedis <47241384+cypherbriedis@users.noreply.github.com>
2020-10-25 21:54:28 +0200

    neopixel GRBW changed to GRB

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2430,7 +2430,7 @@
 // Support for Adafruit NeoPixel LED driver
 #define NEOPIXEL_LED
 #if ENABLED(NEOPIXEL_LED)
-  #define NEOPIXEL_TYPE   NEO_GRBW // NEO_GRBW / NEO_GRB - four/three channel driver type (defined in Adafruit_NeoPixel.h)
+  #define NEOPIXEL_TYPE   NEO_GRB // NEO_GRBW / NEO_GRB - four/three channel driver type (defined in Adafruit_NeoPixel.h)
   #define NEOPIXEL_PIN     P1_24       // LED driving pin
   //#define NEOPIXEL2_TYPE NEOPIXEL_TYPE
   //#define NEOPIXEL2_PIN    5
