2cf24e2b5a6e453ccfe804f05af6fe90ded309f8
cypherbriedis <47241384+cypherbriedis@users.noreply.github.com>
2021-02-21 18:48:31 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -70,7 +70,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Maris Briedis 27-10-2020 SKRv1.4 Turbo)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Maris Briedis 21-02-2021 SKRv1.4 Turbo)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
